323727e6f769e58cfee3852d7e13aff3e0c40440
Frank Moreno <frankmoreno1993@gmail.com>
2017-09-20 07:34:23 -0500

    Agrupando data en un mismo array de json

---
+++ assets/js/data.js
@@ -0,0 +1,23 @@
+var data = [
+  {
+    question:"Which is the oldest airline in the world?",
+    alternatives:["Avianca","KML","Qantas"],
+    anwer:1
+  },{
+    question:"Which is the largest port in the world?",
+    alternatives:["Port of Shanghai","Port of Singapore","Port of Rotterdam"],
+    anwer:0
+  },{
+    question:"What is the longest distance cycling backwards?",
+    alternatives:["89.30 km","675.10 km","337.60 km"],
+    anwer:2
+  },{
+    question:"What is the highest speed ever reached by a school bus?",
+    alternatives:["590 km/h","320 km/h","245 km/h"],
+    anwer:0
+  },{
+    question:"What is the longest car trip on one tank of gas?",
+    alternatives:["2617 km","3568 km","1732 km"],
+    anwer:0
+  }
+];
