c85a96632488ef87520764aad41b7cb692a25540
Frank Moreno <frankmoreno1993@gmail.com>
2017-09-20 23:00:33 -0500

    subiendo data completa

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,23 +1,38 @@
 var data = [
   {
+    img: "assets/img/avion.png",
     question:"Which is the oldest airline in the world?",
     alternatives:["Avianca","KML","Qantas"],
-    anwer:1
+    answer:1
   },{
+    img: "assets/img/barco1.png",
     question:"Which is the largest port in the world?",
     alternatives:["Port of Shanghai","Port of Singapore","Port of Rotterdam"],
-    anwer:0
+    answer:0
   },{
+    img: "assets/img/bicicleta.png",
     question:"What is the longest distance cycling backwards?",
     alternatives:["89.30 km","675.10 km","337.60 km"],
-    anwer:2
+    answer:2
   },{
+    img: "assets/img/bus.png",
     question:"What is the highest speed ever reached by a school bus?",
     alternatives:["590 km/h","320 km/h","245 km/h"],
-    anwer:0
+    answer:0
   },{
+    img: "assets/img/carro.png",
     question:"What is the longest car trip on one tank of gas?",
     alternatives:["2617 km","3568 km","1732 km"],
-    anwer:0
+    answer:0
+  },{
+    img: "assets/img/camion.png",
+    message:"Here are your answers:"
   }
 ];
+
+let index = 0;
+let page;
+let pageN = 5;
+let percent = 0;
+let myAnswers = [];
+let summited = false; 
